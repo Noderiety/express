fecc3015da2558f337c9f91fef5733bc06d0167b
Adam Crabtree <dude@noderiety.com>
2014-12-17 13:28:18 -0800

    Remove semicolons

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 var cluster = require('cluster'),
 		numCPUs = require('os').cpus().length,
-		trycatch = require('trycatch'),
     config = require('./config'),
     App = require('./server/lib/app'),
-    app = new App(config);
+    app = new App(config)
 
-app.configureSync(config);
+require('trycatch')
+app.configureSync(config)
 
 if (process.env.NODE_ENV !== 'production') {
-	// trycatch.configure({'long-stack-traces': true});
+	// trycatch.configure({'long-stack-traces': true})
 }
 
 if (cluster.isMaster) {
   for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    cluster.fork()
   }
 
   cluster.on('exit', function (worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-    cluster.fork();
-  });
+    console.log('worker ' + worker.process.pid + ' died')
+    cluster.fork()
+  })
 } else {
   // Workers can share any TCP connection
 	// For requiring from tests
 	app.initialize()
 		.then(function () {
-			console.log('Express server listening on port ' + app.server.get('port'));
+			console.log('Express server listening on port ' + app.server.get('port'))
 		})
-		.done();
+		.done()
 }
 
 if (require.main !== module) {
-	module.exports = app;
+	module.exports = app
 }
 
 // if (process.env.NODE_ENV !== 'production') {
 // 	// THIS MUST BE THE LAST LINE EXECUTED IN THE MAIN MODULE
-// 	return require('safeguards').noSynchronousIO(module);
+// 	return require('safeguards').noSynchronousIO(module)
 // }
 
 
